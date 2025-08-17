f19d6a98d854c3853faa387e33ef82290b45d8fb
Alireza <alireza.saliminezhad.2001@gmail.com>
2025-08-17 20:09:43 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+﻿let userNameInput = document.querySelector('.username')
+let passwordInput = document.querySelector('.password')
+let modal = document.querySelector('.modal')
+
+function data() {
+    let userNameValue = userNameInput.value
+    let passwordValue = passwordInput.value
+
+    if (userNameValue.length < 5 || passwordValue.length < 5) {
+        // alert('Error')
+        modal.style.background = 'rgb(223, 28, 28)'
+        modal.innerHTML = 'لطفا دوباره سعی کنید'
+        modal.style.display = 'inline'
+    } else {
+        modal.style.background = 'black'
+        modal.innerHTML = 'ورود موفقیت آمیز بود'
+        modal.style.display = 'inline'
+    }
+
+    setTimeout(function () {
+        modal.style.display = 'none'
+    }, 3000)
+}
